b2c062709fe462cc3bf59fe3600b976f3a4e34ed
Isabella <isschwarz@edu.aau.at>
2019-03-05 17:16:31 +0100

    Serverzugriff
    Sockets angelegt

--- app/src/main/java/android/isabellaschwarz/einzelabgabe_11777902/MainActivity.java
+++ app/src/main/java/android/isabellaschwarz/einzelabgabe_11777902/MainActivity.java
@@ -3,7 +3,15 @@ package android.isabellaschwarz.einzelabgabe_11777902;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void senden(View view){
         TextView serverantwort = findViewById(R.id.serverAntwort);
-        serverantwort.setText(R.string.TestAntwort);
+        String matrikelnummer = ((EditText)findViewById(R.id.matrikelnummer)).getText().toString();
+
+        try {
+            Socket clientSocket = new Socket("se2-isys.aau.at",53212);
+            DataOutputStream zumServervomClient = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader vomServerzumClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            zumServervomClient.writeBytes(matrikelnummer);
+            String Antwort = vomServerzumClient.readLine();
+            serverantwort.setText(Antwort);
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
