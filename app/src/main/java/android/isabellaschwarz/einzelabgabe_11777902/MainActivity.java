61896f9af26ec4360b9044ee2e394db0b787610e
Isabella <isschwarz@edu.aau.at>
2019-03-05 16:33:05 +0100

    Testfunktion
    Bei Buttonklick wird das Ausgabefeld automatisch geupdated und gibt Martrikelnummer an Server gesendet aus

--- app/src/main/java/android/isabellaschwarz/einzelabgabe_11777902/MainActivity.java
+++ app/src/main/java/android/isabellaschwarz/einzelabgabe_11777902/MainActivity.java
@@ -2,6 +2,8 @@ package android.isabellaschwarz.einzelabgabe_11777902;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void senden(View view){
+        TextView serverantwort = findViewById(R.id.serverAntwort);
+        serverantwort.setText(R.string.TestAntwort);
     }
 }
